87b86fd3cdad941fdf58a3fae0b31d83a60b9cb9
juan1lopez <juanilpz86@gmail.com>
2024-05-25 20:08:46 -0300

    correccion de errores ortograficos

--- app/mapping/__init__.py
+++ app/mapping/__init__.py
@@ -1,4 +1,4 @@
-from .exercise_schema import ExerciseSchema
+from .excercise_schema import ExcerciseSchema
 from .repetition_schema import RepetitionSchema
 from .user_schema import UserSchema
 from .workout_schema import WorkoutSchema
--- app/mapping/excercise_schema.py
+++ app/mapping/excercise_schema.py
@@ -1,12 +1,12 @@
-from app.models import Exercise
+from app.models import Excercise
 from marshmallow import fields, Schema, post_load
 
-class ExerciseSchema(Schema):
+class ExcerciseSchema(Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
     description = fields.String(required=True)
     workout_id = fields.Integer(required=True)
 
     @post_load
-    def make_exercise(self, data, **kwargs):
-        return Exercise(**data)
+    def make_excercise(self, data, **kwargs):
+        return Excercise(**data)
